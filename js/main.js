850b149f461a25a2e2590fc9246db9b1bb5225d8
Lauren Fernandez <lfernan60@ufl.edu>
2018-04-24 21:33:35 -0400

    Add countdown Jquery

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-$('.map')
-     .gmap3({
-       center:[48.8620722, 2.352047],
-       zoom:4
-     })
-     .marker([
-       {position:[48.8620722, 2.352047]},
-       {address:"86000 Poitiers, France"},
-       {address:"66000 Perpignan, France", icon: "http://maps.google.com/mapfiles/marker_grey.png"}
-     ])
-     .on('click', function (marker) {
-       marker.setIcon('http://maps.google.com/mapfiles/marker_green.png');
-     });
+$('#clock').countdown('2018/10/01', function(event) {
+  $(this).html(event.strftime('%D days %H:%M:%S'));
+});
