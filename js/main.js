2f09155dee239af50e60d753e5578eb65f57723e
Lauren Fernandez <lfernan60@ufl.edu>
2018-04-25 22:17:44 -0400

    Add custom Javascript

--- js/main.js
+++ js/main.js
@@ -4,11 +4,33 @@ $('#clock').countdown('2018/10/01', function(event) {
   $(this).html(event.strftime('%D days %H:%M:%S'));
 });
 
-//Written Javascript turns selected H1's teal on click
+//Written jQuery turns selected H1's teal on click
 function myFunction() {
     document.getElementById("demo").style.color = "#0192b3";
 }
 
+//Presents loading screen on home page until clicked
 $('body').loading({
   stoppable: true
 });
+
+//Javascript modal
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
