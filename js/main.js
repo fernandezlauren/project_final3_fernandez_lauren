41015e9638076a0453a766eba5a4734cf30d8344
Lauren Fernandez <lfernan60@ufl.edu>
2018-04-25 20:34:17 -0400

    Add javascript to change color on click

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@
 $('#clock').countdown('2018/10/01', function(event) {
   $(this).html(event.strftime('%D days %H:%M:%S'));
 });
+
+//Written Javascript turns selected H1's teal on click
+function myFunction() {
+    document.getElementById("demo").style.color = "#0192b3";
+}
